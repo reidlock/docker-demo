03ace86a96b9c2a9b3f81174d31a95faba2c7708
Reid Wilkes <reidwilkes@gmail.com>
2016-03-12 15:53:55 -0800

    foo

--- rundemo.sh
+++ rundemo.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 #first build the docker images
+#
 pushd SimpleWebServer
 serverimageid=$(docker build -q -t "simplewebserver:latest" .)
 popd
-#we are going to build a base client image which installs curl.
-#this is a lengthier operation, so building the image here means it will
-#be quicker to run this script subsequent times with changes to the client and/or server
+# we are going to build a base client image which installs curl.
+# this is a lengthier operation, so building the image here means it will
+# be quicker to run this script subsequent times with changes to the client and/or server
 pushd ClientBase
 docker build -q -t "reidlock-clientbase" .
 popd
@@ -15,9 +16,10 @@ clientimageid=$(docker build -q -t "testclient:latest" .)
 popd
 
 #now set up the network the containers will join
-# first remove old network if exists
+# first check if the network already exists
 netid=$(docker network ls -q -f name=reidlock-demo-network)
 if [ -z $netid ]; then
+# then create the network if needed
   netid=$(docker network create --internal reidlock-demo-network)
 fi
 
@@ -27,9 +29,8 @@ servercontainerid=$(docker run -d --net=reidlock-demo-network --name=reidlock-de
 #sleep just long enough to give the server time to come up
 sleep 2
 
-#now start the client container and check result
+#now start the client container
 res=$(docker run --net=reidlock-demo-network --name=reidlock-testclient testclient reidlock-demo-server 8080)
-echo test $res
 
 #clean things up
 docker stop $servercontainerid
@@ -39,3 +40,6 @@ docker rmi $serverimageid
 clientcontainerid=$(docker ps -aqf name=reidlock-testclient)
 docker rm $clientcontainerid
 docker rmi $clientimageid
+
+#output result
+echo test $res
